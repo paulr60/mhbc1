50ba40984020f31160d52b3dd9dfa8b5f38841b5
Paul Roush <proush@gmail.com>
2012-11-15 21:33:22 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mhbc1::Application.config.secret_token = '1b730509fdd9b7e18a045ccc7b2e7124be8a7b9b46a626fc819f4ea2c417ba866358e6f71f7047b8d9b71b61d9a91f8e52c90dfb0667d3030599e18e423e7e73'
