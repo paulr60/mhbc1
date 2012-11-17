1cb868fda8c0b7ed757a09a36a7b79472ad26e4f
Paul Roush <proush@gmail.com>
2012-11-17 14:35:42 -0500

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Mhbc1::Application.routes.draw do
-  get "static_pages/home"
+  get "users/new"
+
+  root to: 'static_pages#home'
+
+  match '/signup',  to: 'users#new'
+  
+  match '/contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,10 +4,17 @@ describe "StaticPages" do
 	subject { page }
 
   	describe "Home page" do
-  		before { visit '/static_pages/home' }
+  		before { visit root_path }
   		let(:mhbc) { 'Montgomery Hills Baptist Church' }
 
   		it { should have_selector('h1',		text: "#{mhbc}") }
   		it { should have_selector('title',	text: "#{mhbc}") }
   	end
+
+  	describe "Contact Page" do
+  		before { visit contact_path }
+
+  		it { should have_selector('h1', text: 'Contact') }
+  	end
+  	
 end
