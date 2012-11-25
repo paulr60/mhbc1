94222f316c068ab9173935b379964ed9c1e84729
Paul Roush <proush@gmail.com>
2012-11-25 09:36:00 -0500

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,13 +25,32 @@ describe User do
     it { should respond_to(:password) }
     it { should respond_to(:password_confirmation) }
     it { should respond_to(:remember_token) }
+    it { should respond_to(:admin) }
     it { should respond_to(:authenticate) }
 
     it { should be_valid }
+    it { should_not be_admin }
+
+    describe "accessible attributes" do
+        it "should not allow access to admin field" do
+            expect do
+                User.new(admin: true)
+            end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+        end
+    end
+
+    describe "with admin attribute set to 'true'" do
+        before do
+            @user.save!
+            @user.toggle!(:admin)
+        end
+
+        it { should be_admin }
+    end
 
     describe "when name is not present" do
-		    before { @user.name = "" }
-		    it { should_not be_valid }
+		before { @user.name = "" }
+		it { should_not be_valid }
     end
 
     describe "when name is too long" do
@@ -45,14 +64,14 @@ describe User do
     end
 
     describe "when email format is invalid" do
-		    it "should be invalid" do
+		it "should be invalid" do
             addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-  						            foo@bar_baz.com foo@bar+baz.com]
+  			       			foo@bar_baz.com foo@bar+baz.com]
             addresses.each do |invalid_address|
                 @user.email = invalid_address
                 @user.should_not be_valid
             end
-  		  end
+  		end
   	end
 
   	describe "when email format is valid" do
