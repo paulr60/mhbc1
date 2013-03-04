66b9b39e267bbd5e3c22aabf9fb8c668d62656f4
Paul Roush <proush@gmail.com>
2013-03-04 17:41:53 -0500

    Cleanup from removing photo-uploader from articles

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,19 +1,23 @@
 module PhotosHelper
 
-	def photo_version(name, version)
-    	p = Photo.find_by_name(name)
-    	p ? p.image_url(version) : nil
-    end
-
-    def photo_tag_to_original(name, version)
-    	debugger
-    	return "" if name.blank?
-
+    def photo_object(name)
     	if name.class == Photo 		# Arg is Photo obj, not name of photo
     		p = name
     	else
 	    	p = Photo.find_by_name(name)
 	    end
+        return p
+    end
+
+	def photo_version(name, version)
+    	p = photo_object(name)
+    	p ? p.image_url(version) : nil
+    end
+
+    def photo_tag_to_original(name, version)
+    	return "" if name.blank?
+
+        p = photo_object(name)
     	return "" if !p
 
     	link_to(image_tag(p.image_url(version)), p.image_url)
